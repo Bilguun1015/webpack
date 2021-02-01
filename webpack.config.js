cea4167e72d3757431bdcf0a9ab9d6b28604a9cb
Bilguun Nomch <bbg.1015@gmail.com>
2021-02-01 11:39:13 -0600

    added MD5 hash to bundle names, also used clean-webpack-plugin to clean before each build

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, './dist'),
     // publicPath: 'http://some-cdn.com/',
     publicPath: 'dist/',
@@ -59,6 +60,13 @@ module.exports = {
   },
   plugins: [
     new TerserPlugin(),
-    new MiniCssExtractPlugin({ filename: 'style.css' }),
+    new MiniCssExtractPlugin({ filename: 'style.[contenthash].css' }),
+    // adding cleanup for the builder outside of dist
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [
+        '**/*',
+        path.join(process.cwd(), 'build/**/*'),
+      ],
+    }),
   ],
 };
